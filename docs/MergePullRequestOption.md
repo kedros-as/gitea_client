081fc0a2946ba70ddeeb1872d42337fd72701a44
Juraj Hrubša <juraj.hrubsa@kedros.sk>
2020-04-14 15:54:55 +0200

    First commit.

---
+++ docs/MergePullRequestOption.md
@@ -0,0 +1,13 @@
+# MergePullRequestOption
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**do** | **str** |  | 
+**merge_message_field** | **str** |  | [optional] 
+**merge_title_field** | **str** |  | [optional] 
+**force_merge** | **bool** |  | [optional] 
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
---
+++ gitea_client/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from gitea_client.api.admin_api import AdminApi
+from gitea_client.api.issue_api import IssueApi
+from gitea_client.api.miscellaneous_api import MiscellaneousApi
+from gitea_client.api.notification_api import NotificationApi
+from gitea_client.api.organization_api import OrganizationApi
+from gitea_client.api.repository_api import RepositoryApi
+from gitea_client.api.user_api import UserApi
